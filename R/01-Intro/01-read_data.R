56cb2721867137f8fbea15cc8722e79343b98e8f
Lennart Klein <31488915+kleinlennart@users.noreply.github.com>
2022-11-25 10:01:06 +0100

    read

---
+++ R/01-Intro/01-read_data.R
@@ -0,0 +1,9 @@
+### Getting data into R ###
+
+# Load the tidyverse library
+library(tidyverse)
+
+raw <- readRDS("data/TWLZ_Seminar_Sample.rds")
+
+## For CSVs use:
+# file <- read.csv("data/your_file.csv")
